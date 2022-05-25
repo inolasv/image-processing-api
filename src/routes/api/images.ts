24691972a28a443ff850fe105a44e02338492b9a
Saloni Vaishnav <inolasv@gmail.com>
2022-05-25 16:34:38 -0500

    fixed typescript and package.json

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -5,15 +5,12 @@ import validateInput from '../../utilities/validateInputs';
 // create the images route
 const images = express.Router();
 
-// declare variables needed
-let width: number;
-let height: number;
-let filename: string | null;
-
 // function to display the image
 const displayImage = async (
     res: express.Response,
-    filename: string
+    filename: string,
+    width: number,
+    height: number
 ): Promise<void> => {
     const new_filename = await resizeImage(filename, width, height);
     const new_src = `http://localhost:3000/${new_filename.substring(8)}`;
@@ -26,14 +23,14 @@ const displayImage = async (
 // routes to the file and handles the queries
 images.get('/', async (req, res) => {
     // res.send("image connected");
-    filename = req.query.filename as string | null;
-    width = Number(req.query.width);
-    height = Number(req.query.height);
+    const filename: string | null = req.query.filename as string | null;
+    const width: number = Number(req.query.width);
+    const height: number = Number(req.query.height);
 
     // validate inputs
     const valid = await validateInput(filename, width, height);
     if (valid == null) {
-        displayImage(res, filename as string);
+        displayImage(res, filename as string, width, height);
     } else {
         res.send(valid);
     }
